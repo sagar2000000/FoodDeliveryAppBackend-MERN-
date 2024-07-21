701695da7a91ebaedc06cb237f4d1a5811d8a141
sagar2000000 <sagar171joshi@gmail.com>
2024-07-22 00:12:36 +0545

    db updated

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB= async()=>{
-  await mongoose.connect("mongodb+srv://sagar299:sagares123@cluster0.yb6gpss.mongodb.net/?retryWrites=true&w=majority&appName=Cluster")
+  await mongoose.connect("mongodb+srv://sagar299:{}@cluster0.yb6gpss.mongodb.net/?retryWrites=true&w=majority&appName=Cluster")
   .then(()=>{
     console.log("DB connected sucessfully")
   })
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -11,4 +11,7 @@ userRouter.post('/register',registerUser)
 userRouter.post('/login',loginUser)
 
 
+
+
+
 export default userRouter
